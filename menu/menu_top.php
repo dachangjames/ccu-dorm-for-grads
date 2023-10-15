0f8ca4c669af249389247be34c0d43da18bbbae8
adamaconguli <waltson2003@gmail.com>
2023-10-15 11:13:42 +0800

    added banner, completed menu_top.php

--- menu/menu_top.php
+++ menu/menu_top.php
@@ -6,31 +6,33 @@
   <!-- links navigation -->
   <ul id="top-links">
     <li>
-      <a href="#">link1</a>
+      <a href="index.php">回首頁</a>
     </li>
     <li>
-      <a href="#">link2</a>
+      <a href="https://www.ccu.edu.tw/" target="_blank">中正大學</a>
     </li>
     <li>
-      <a href="#">link3</a>
+      <a href="https://studaffairs.ccu.edu.tw/" target="_blank">學生事務處</a>
     </li>
     <li>
-      <a href="#">link4</a>
+      <a href="https://it.ccu.edu.tw/" target="_blank">資訊處</a>
     </li>
     <li>
-      <a href="#">link5</a>
+      <a href="https://www.dorm.ccu.edu.tw/" target="_blank">宿舍網路</a>
     </li>
     <li>
-      <a href="#">link6</a>
+      <a href="https://www026185.ccu.edu.tw/dormrepair/" target="_blank">宿舍維修申請</a>
     </li>
     <li>
-      <a href="#">link7</a>
+      <a href="index.php?inner=map" target="_top">宿舍平面圖</a>
     </li>
     <li>
-      <a href="#">link8</a>
+      <a href="https://www026185.ccu.edu.tw/graddorm/doc/graddorm_guide.ppt">宿舍導覽</a>
     </li>
     <li>
-      <a href="#">link9</a>
+      <a href="index.php?inner=faq" target="_top">常見問答</a>
     </li>
   </ul>
+  <img id="top-banner" src=<?php echo "pic/banner" . rand(1, 4) . ".jpg"; ?> >
+  <div style="width: 100vw; height: 12px; background: lightgrey;"></div>
 </nav>
