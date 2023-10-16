3379eda3bf0c5787ab0ec5db1d19b7e76f8b443c
adamaconguli <waltson2003@gmail.com>
2023-10-16 09:53:48 +0800

    Optimized page protection. <ALPHA CODE>

--- include/lib_logout.php
+++ include/lib_logout.php
@@ -2,6 +2,10 @@
 // clear cookie
 setcookie("jwt", "", time(), "/");
 
+// clear session
+unset($_SESSION["account"]);
+unset($_SESSION["perm"]);
+
 // redirect to home page
 header("location: /");
 exit;
--- include/lib_transpage.php
+++ include/lib_transpage.php
@@ -1,36 +1,63 @@
 <?php
 // transpage logic
 
-// check if user is logged in
-if (isset($_SESSION["account"])) {
-  if (isset($_GET["inner"])) {
-    // check user permission
-    if ($_SESSION["perm"] === substr($_GET["inner"], 0, 3)) {
-      // check if the page exist
-      $filepath = $_SESSION["perm"] . "/" . $_GET["inner"] . ".php";
-      if (file_exists($filepath)) {
-        include $filepath;
-      } else {
-        include "page/404.html";
-      }
-    } else {
-      include "page/403.html";
-    }
-  } else {
-    // perm/perm_index.php
-    include $_SESSION["perm"] . "/" . $_SESSION["perm"] . "_index.php";
-  }
-} else if (isset($_GET["inner"])) {
-  // logged out
+
+/**
+ *         //\\         ||                  |||||||||||||\\     ||              ||         //\\
+ *        //  \\        ||                  ||           \\\    ||              ||        //  \\
+ *       //    \\       ||                  ||             ||   ||              ||       //    \\
+ *      //      \\      ||                  ||           ///    ||              ||      //      \\
+ *     //||||||||\\     ||                  |||||||||||||//     ||||||||||||||||||     //||||||||\\
+ *    //          \\    ||                  ||                  ||              ||    //          \\
+ *   //            \\   ||                  ||                  ||              ||   //            \\
+ *  //              \\  ||||||||||||||||||  ||                  ||              ||  //              \\
+ */
+
+// EDIT WITH CATION!!!
+
+$perms = ["adm", "qry", "dep", "stu"];
+
+if (isset($_GET["inner"])) {
+  // everyone can access
   switch ($_GET["inner"]) {
+    // switch through pages everyone can access
     case "login":
       include "page/login.html";
       break;
     default:
-      include "page/404.html";
-      break;
+      // check if the user is logged in
+      if (isset($_SESSION["account"])) {
+        // check if the file exist
+        $filepath = substr($_GET["inner"], 0, 3) . "/" . $_GET["inner"] . ".php";
+        if (file_exists($filepath)) {
+          // check if the user has the right permission
+          if ($_SESSION["perm"] === substr($_GET["inner"], 0, 3)) {
+            include $filepath;
+          } else {
+            // 403 Forbidden
+            include "page/403.html";
+          }
+        } else {
+          // 404 Not Found
+          include "page/404.html";
+        }
+      } else if (isset($_GET["inner"]) && in_array(substr($_GET["inner"], 0, 3), $perms)) {
+        // logged out user try to access perms pages
+        // 401 Unauthorized
+        include "page/401.html";
+      } else {
+        // 404 Not Found
+        include "page/404.html";
+      }
   }
 } else {
-  include "div/div_home.php";
+  // entry pages
+  if (isset($_SESSION["account"])) {
+    // {perm}/{perm}_index.php
+    include $_SESSION["perm"] . "/" . $_SESSION["perm"] . "_index.php";
+  } else {
+    include "div/div_home.php";
+  }
 }
+
 ?>
