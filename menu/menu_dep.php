d1ca808ab103dff2e5df08581f6bf8a3fff116e9
adamaconguli <waltson2003@gmail.com>
2023-10-17 09:57:54 +0800

    Added new perms logic in lib_transpage

--- menu/menu_dep.php
+++ menu/menu_dep.php
@@ -1,15 +1,15 @@
 <ul>
   <li>
-    <a href="/?inner=depquery">系所名額查詢</a>
+    <a href="/?inner=depquery">系所名額查詢X</a>
   </li>
   <li>
-    <a href="/?inner=depapply">住宿資格申請</a>
+    <a href="/?inner=depapply">住宿資格申請X</a>
   </li>
   <li>
-    <a href="/?inner=depdel">放棄床位申請</a>
+    <a href="/?inner=depdel">放棄床位申請X</a>
   </li>
   <li>
-    <a href="/?inner=depdelquery">確認放棄查詢</a>
+    <a href="/?inner=depdelquery">確認放棄查詢X</a>
   </li>
   <li>
     <a href="/">回首頁</a>
