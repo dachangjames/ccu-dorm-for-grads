7fbb129ec0d2c55a9ef9dbd3b4ee7f0c6ad2f915
adamaconguli <waltson2003@gmail.com>
2023-10-19 14:35:21 +0800

    formatted applyfor, applynote, member, guide

--- menu/menu.php
+++ menu/menu.php
@@ -1,21 +1,21 @@
 <ul>
   <li>
-    <a href="/?inner=applyfor">宿舍申請流程X</a>
+    <a href="/?inner=applyfor">宿舍申請流程</a>
   </li>
   <li>
     <a href="/?inner=time">相關申請期限X</a>
   </li>
   <li>
-    <a href="/?inner=applynote">申請注意事項X</a>
+    <a href="/?inner=applynote">申請注意事項</a>
   </li>
   <li>
-    <a href="/?inner=rule">宿舍相關規範X</a>
+    <a href="/?inner=rule">宿舍相關規範</a>
   </li>
   <li>
     <a href="/?inner=map">平面配置查詢X</a>
   </li>
   <li>
-    <a href="/?inner=guide">宿舍概況簡介X</a>
+    <a href="/?inner=guide">宿舍概況簡介</a>
   </li>
   <li>
     <a href="/">公告及登入系統</a>
