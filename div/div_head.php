e2bb6e2f57ee650de89aaf1259867ef78b68fe4d
adamaconguli <waltson2003@gmail.com>
2023-10-20 14:54:39 +0800

    style added to inner pages

--- div/div_head.php
+++ div/div_head.php
@@ -2,8 +2,10 @@
   <!-- site configs -->
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- icons -->
   <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
   <link href="css/style.css" rel="stylesheet" type="text/css">
   <link href="css/menu.css" rel="stylesheet" type="text/css">
+  <link href="css/inner.css" rel="stylesheet" type="text/css">
   <title>國立中正大學-碩博士宿舍管理系統</title>
 </head>
