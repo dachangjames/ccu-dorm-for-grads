25d579abeadfe4ff5f45d4dee0ec41a2afb4ebd3
adamaconguli <waltson2003@gmail.com>
2023-11-26 22:04:19 +0800

    optimized admset_dep_unit

--- adm/admset_dep.php
+++ adm/admset_dep.php
@@ -1,52 +1,59 @@
 <?php
-    if(isset($_POST["dep"])){
-        header("location:/?inner=admset_dep_unit");
-    }
+if (isset($_POST["dep"])) {
+  header("location:/?inner=admset_dep_unit");
+}
 ?>
 <div class="inner">
-    <div class="inner-subtitle">系所申請住宿名額管理</div>
-    <form action="/?inner=admset_dep_unit" method="post">
-        <table class="inner-table">
-            <tr style="font-weight: bold;">
-                <td align="center">#</td>
-                <td align="center">學院</td>
-                <td align="center">操作</td>
-            </tr>
-            <tr>
-                <td align="center">1</td>
-                <td align="center">1 文學院</td>
-                <td align="center">
-                    <button type="submit" name="dep" value="1">設定</button>
-                </td>
-            </tr>
-            <tr>
-                <td align="center">2</td>
-                <td align="center">2 理學院</td>
-                <td align="center">
-                    <button type="submit" name="dep" value="2">設定</button>
-                </td>
-            </tr>
-            <tr>
-                <td align="center">3</td>
-                <td align="center">3 工學院</td>
-                <td align="center">
-                    <button type="submit" name="dep" value="3">設定</button>
-                </td>
-            </tr>
-            <tr>
-                <td align="center">4</td>
-                <td align="center">4 社會科學院</td>
-                <td align="center">
-                    <button type="submit" name="dep" value="4">設定</button>
-                </td>
-            </tr>
-            <tr>
-                <td align="center">5</td>
-                <td align="center">5 法學院</td>
-                <td align="center">
-                    <button type="submit" name="dep" value="5">設定</button>
-                </td>
-            </tr>
-        </table>
-    </form>
+  <p class="inner-nav">
+    <a href="/" class="link">回首頁</a>
+    /
+    <span>系所申請住宿名額管理</span>
+  </p>
+  <div class="inner-subtitle">
+    <p>系所申請住宿名額管理</p>
+  </div>
+  <form action="/?inner=admset_dep_unit" method="post">
+    <table class="inner-table">
+      <tr style="font-weight: bold;">
+        <td align="center">#</td>
+        <td align="center">學院</td>
+        <td align="center">操作</td>
+      </tr>
+      <tr>
+        <td align="center">1</td>
+        <td align="center">1000 文學院</td>
+        <td align="center">
+          <button type="submit" name="dep" value="1000">設定</button>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">2</td>
+        <td align="center">2000 理學院</td>
+        <td align="center">
+          <button type="submit" name="dep" value="2000">設定</button>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">3</td>
+        <td align="center">3000 工學院</td>
+        <td align="center">
+          <button type="submit" name="dep" value="3000">設定</button>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">4</td>
+        <td align="center">4000 社會科學院</td>
+        <td align="center">
+          <button type="submit" name="dep" value="4000">設定</button>
+        </td>
+      </tr>
+      <tr>
+        <td align="center">5</td>
+        <td align="center">5000 法學院</td>
+        <td align="center">
+          <button type="submit" name="dep" value="5000">設定</button>
+        </td>
+      </tr>
+    </table>
+  </form>
 </div>
--- adm/admset_dep_unit.php
+++ adm/admset_dep_unit.php
@@ -1,72 +1,71 @@
 <div class="inner">
-    <div class="inner-subtitle">
-        <p>系所申請名額設定</p>
-        <p class="inner-nav">
-            <a href="/?inner=admset_dep" class="link">回上頁</a>
-        </p>
-	</div>
-	<?php 
-	$deps = [
-		1 => "中國文學研究所",
-		2 => "數學研究所",
-		3 => "資訊工程研究所",
-		4 => "心理研究所",
-		5 => "法律研究所"
-	];
-	foreach($deps as $dNum => $dName){
-        echo "<form action='/?inner=admset_dep_unit' method='post' id=$dNum style='display:none'>";
-        echo "	<table class='inner-table'>";
-        echo "		<tr style='font-weight: bold;'>";
-        echo "			<td align='center'>#</td>";
-        echo "			<td align='center'>系所單位</td>";
-        echo "			<td align='center'>男生名額</td>";
-        echo "			<td align='center'>女生名額</td>";
-        echo "		</tr>";
-        echo "		<tr>";
-        echo "			<td align='center'>$dNum</td>";
-        echo "			<td align='center'>$dNum $dName</td>";
-        echo "			<td align='center'>";
-        echo "				<input type='number' name='num_m_$dNum' placeholder='0' required>";
-        echo "				<label>人</label>";
-        echo "			</td>";
-        echo "			<td align='center'>";
-        echo "				<input type='number' name='num_f_$dNum' placeholder='0' required>";
-        echo "				<label>人</label>";
-        echo "			</td>";
-        echo "		</tr>";
-        echo "		<tr>";
-        echo "			<td align='center' colspan='2'>";
-        echo "				<input type='submit' name='Quota' value='Submit' class='action-button'>";
-        echo "			</td>";
-        echo "			<td align='center' colspan='2'>";
-        echo "				<input type='reset' value='Clear' class='action-button'>";
-        echo "			</td>";
-        echo "		</tr>";
-        echo "	</table>";
-        echo "</form>";
-	}
-?>
+  <p class="inner-nav">
+    <a href="/" class="link">回首頁</a>
+    /
+    <a href="/?inner=admset_dep" class="link">系所申請住宿名額管理</a>
+    /
+    <span>系所申請名額設定</span>
+  </p>
+  <div class="inner-subtitle">
+    <p>系所申請名額設定</p>
+  </div>
+  <?php
+  $deps = [
+    "1000" => "中國文學研究所",
+    "2000" => "數學研究所",
+    "3000" => "資訊工程研究所",
+    "4000" => "心理研究所",
+    "5000" => "法律研究所"
+  ];
 
-<?php
-	$upd_Table = "sl8gdm_dep";
-	if(isset($_POST["dep"])){
-		$idx = $_POST["dep"];
-		echo "<script>document.getElementById('$idx').style.display='block';</script>";
-		$_SESSION["dep"] = $_POST["dep"];
-	}
+  if (isset($_POST["dep"])) {
+    $dep = $_POST["dep"];
+    $_SESSION["dep"] = $dep;
+  } else if (isset($_POST["quota"]) && isset($_SESSION["dep"])) {
+    $upd_Table = "sl8gdm_dep";
+    $dep = $_SESSION["dep"];
 
-    // $row=DB::fetch_row("sl8gdm_dep", "unit_parent", $_SESSION["dep"]*1000);
-    // foreach($row as $key => $value){
-    //     echo "{$key} : {$value}<br>";
-    // }
-
-	if(isset($_POST["Quota"])){
-		$key = $_SESSION["dep"];
-		$num_m = $_POST["num_m_$key"];
-		$num_f = $_POST["num_f_$key"];
-		$upd_Row = array("unit_parent" => $key * 1000);
-		DB::update_row($upd_Table, $upd_Row, ["a_num_m" => $num_m]);
-		DB::update_row($upd_Table, $upd_Row, ["a_num_f" => $num_f]);
-		echo "<script>location.replace('/?inner=admset_dep');</script>";
-	}
-?>
+    if (isset($_POST["quota"])) {
+      var_dump($_POST);
+      $num_m = $_POST["num_m_$dep"];
+      $num_f = $_POST["num_f_$dep"];
+      $upd_Row = array("unit_parent" => $dep);
+      DB::update_row($upd_Table, $upd_Row, ["a_num_m" => $num_m]);
+      DB::update_row($upd_Table, $upd_Row, ["a_num_f" => $num_f]);
+      header("location: /?inner=admset_dep");
+      exit;
+    }
+  } else {
+    die();
+  }
+  ?>
+  <form action="/?inner=admset_dep_unit" method="post">
+    <table class="inner-table">
+      <tr style="font-weight: bold;">
+        <td align="center">#</td>
+        <td align="center">系所單位</td>
+        <td align="center">男生名額</td>
+        <td align="center">女生名額</td>
+      </tr>
+      <tr>
+        <td align="center"><?php echo $dep ?></td>
+        <td align="center"><?php echo $dep . " " . $deps[$dep] ?></td>
+        <td align="center">
+          <input type="number" name=<?php echo "num_m_$dep" ?> placeholder="0" required>
+          <label>人</label>
+        </td>
+        <td align="center">
+          <input type="number" name=<?php echo "num_f_$dep" ?> placeholder="0" required>
+          <label>人</label>
+        </td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">
+          <button type="submit" name="quota" class="action-button">Submit</button>
+        </td>
+        <td align="center" colspan="2">
+          <button type="reset" class="action-button">Clear</button>
+        </td>
+      </tr>
+    </table>
+  </form>
