570b2e960621aeeb9fbfc4def79e3a5f13dfcd54
Ethan Lai <ethan.418.63@gmail.com>
2023-12-18 14:26:15 +0800

    Update admappwait.php

--- adm/admappwait.php
+++ adm/admappwait.php
@@ -1,1 +1,34 @@
-<h1>admappwait</h1>
+<div class="inner">
+    <p class="inner-nav">
+		<a href="/" class="link">回首頁</a>
+		/
+		<span>遞補作業系統</span>
+	</p>
+	<div class="inner-subtitle">
+		<p>遞補作業系統選單</p>
+	</div>
+    <div class="inner-content admappwait">
+		<table class="inner-table" border=1>
+			<tr>
+				<th>選項</th>
+				<th>操作</th>
+			</tr>
+			<tr>
+				<td>放棄名單</td>
+				<td><a href="/?inner=admappwait_del" class="link">查看</a></td>
+			</tr>
+			<tr>
+				<td>抽籤名單</td>
+				<td>
+					<a href="/?inner=admappwait_numed" class="link">查看</a>
+					<a href="/?inner=admappwait_num" class="link">前往抽籤</a>
+				</td>
+			</tr>
+        </table>
+    </div>
+	<style>
+		.admappwait .inner-table{
+			table-layout : auto;
+		}
+	</style>
+</div>
