177646280f20ebb5f23d54acb592c386eb1dc546
adamaconguli <waltson2003@gmail.com>
2023-12-23 12:09:33 +0800

    finalize footer css

--- div/div_footer.php
+++ div/div_footer.php
@@ -1,9 +1,9 @@
 <!-- footer component -->
 <footer id="bottom">
-  <p>Copyright &copy; 2023
-    <a href="https://github.com/ccu-dorm-for-grads/ccu-dorm-for-grads">Source</a>
+  <p>Copyright &copy; 2023 &nbsp;
+    <a class="link" href="https://github.com/ccu-dorm-for-grads/ccu-dorm-for-grads">Source</a>
     , &nbsp;
-    <a href="https://www.ccu.edu.tw/">國立中正大學</a>
+    <a class="link" href="https://www.ccu.edu.tw/">國立中正大學</a>
     . &nbsp;
     All right reserved.
   </p>
